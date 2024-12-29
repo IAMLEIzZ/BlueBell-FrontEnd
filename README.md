cb042709e030b68a0c529bda4d723f22a02395fd
IAMLEIzZ <1399311818@qq.com>
2024-12-29 15:48:45 +0800

    修复启动 ESLInt 报错

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# BlueBell-FrontEnd
-BlueBell前端代码
+# bluebell_frontend
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    assetsDir: "static",
-    devServer: {
-        proxy: {
-            '/api/v1': {
-              target: 'http://127.0.0.1:8081',
-              changeOrigin: true,
-            }
-        }
-    }
-  }
+  assetsDir: "static",
+  devServer: {
+    proxy: {
+      "/api/v1": {
+        target: "http://127.0.0.1:8081",
+        changeOrigin: true,
+      },
+    },
+  },
+  lintOnSave: false,
+};
